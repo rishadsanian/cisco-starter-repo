8d1205dbd00d38546553b6289ec14fab1a1260d2
Rishad Alam <rishad.s.a@gmail.com>
2023-10-25 16:21:09 -0400

    task 3 - ip address request

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import Exhibit from "./components/Exhibit";
+import GetIP from "./api/getIp";
 
 function App() {
-  const widget1 = { heading: "Widget1", data: "Data1", type:"sm" };
-  const widget2 = { heading: "Widget2", data: "Data2" };
+  const { ipv4, ipv6 } = GetIP();
+  // console.log(`ipv4:${ipv4}, ipv6: ${ipv6}`);
+
+  const widget1 = { heading: "Public IPV4 Address ", data: ipv4 };
+  const widget2 = { heading: "Public IPV6 Address", data: ipv6 };
   const widget3 = { heading: "Widget3", data: "Data3" };
   const widget4 = { heading: "Widget4", data: "Data4" };
+
   return (
     <div className="App">
       <Banner />
---
+++ sextant/src/api/getIp.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+// Function to get your public IPv4 address
+async function getIP(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data.ip;
+  } catch (error) {
+    console.error("Error fetching IP address:", error);
+    return null;
+  }
+}
+
+// Function to get both IPv4 and IPv6 addresses
+
+function GetIP() {
+  const [ipv4, setIPv4] = useState(null);
+  const [ipv6, setIPv6] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const ipv4Address = await getIP("https://api.ipify.org?format=json");
+      const ipv6Address = await getIP("https://api64.ipify.org?format=json");
+      setIPv4(ipv4Address);
+      setIPv6(ipv6Address);
+      console.log(ipv4Address, ipv6Address);
+    }
+    fetchData();
+  }, []);
+
+  return { ipv4, ipv6 };
+}
+
+export default GetIP;
