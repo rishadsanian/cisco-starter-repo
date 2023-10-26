8decafcd888fbc3880355aa5ca3400961ca31305
Rishad Alam <rishad.s.a@gmail.com>
2023-10-26 02:33:49 -0400

    different style

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import Banner from "./components/Banner";
 import Exhibit from "./components/Exhibit";
+import LatencyChart from "./components/LatencyChart";
+
 import GetIP from "./api/getIp";
 import PacketLatency from "./api/packetLatency";
 
 function App() {
   const { ipv4, ipv6 } = GetIP();
-  const {latency} = PacketLatency();
+  const { latency } = PacketLatency();
 
   const widget1 = { heading: "Public IPV4 Address ", data: ipv4 };
   const widget2 = { heading: "Public IPV6 Address", data: ipv6 };
-  const widget3 = { heading: "Latency", data: latency + " ms" };
+  const widget3 = { heading: "Latency (ms)", data: latency };
   const widget4 = { heading: "Widget4", data: "Data4" };
 
   return (
@@ -21,7 +23,8 @@ function App() {
         <Exhibit widget={widget1} />
         <Exhibit widget={widget2} />
         <Exhibit widget={widget3} />
-        <Exhibit widget={widget4} />
+        {/* <LatencyChart /> */}
+        {/* <Exhibit widget={widget4} /> */}
       </div>
     </div>
   );
--- sextant/src/components/Exhibit.jsx
+++ sextant/src/components/Exhibit.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Exhibit.css";
 
 function Exhibit({ widget }) {
+  const [statusSymbolClass, setStatusSymbolClass] = useState("");
+
+  useEffect(() => {
+    // Determine the class for the status symbol based on widget.data
+    if (widget.data !== null && widget.data !== undefined) {
+      setStatusSymbolClass("status-symbol-green");
+    } else {
+      setStatusSymbolClass("status-symbol-red");
+    }
+  }, [widget.data]);
+
   return (
     <div className="widget">
-      <h2 className="heading">{widget.heading}</h2>
-      <p className="value">{widget.data}</p>
+      <header>
+        <div className={`status-symbol ${statusSymbolClass}`}></div>
+        <h2 className="heading">{widget.heading}</h2>
+      </header>
+      {widget.data !== null && widget.data !== undefined ? (
+        <p className="value">{widget.data}</p>
+      ) : (
+        <div className="status-symbol"></div>
+      )}
     </div>
   );
 }
