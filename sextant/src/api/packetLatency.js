2a7d27e1ad1037c38e550e356eca0aaa0a3f3cd6
Rishad Alam <rishad.s.a@gmail.com>
2023-10-26 16:23:07 -0400

    statuslight fix

--- sextant/src/api/packetLatency.js
+++ sextant/src/api/packetLatency.js
@@ -5,30 +5,30 @@ function PacketLatency() {
   const [latency, setLatency] = useState(null);
   const [latencyStatus, setLatencyStatus] = useState("red");
   useEffect(() => {
-    // Connect to the WebSocket server
     const client = new w3cwebsocket('ws://localhost:55455');
 
     // Handle incoming messages from the server
+
+    // connect to the server
     client.onopen = () => {
-      // console.log('WebSocket client connected');
       setLatencyStatus("green");
     };
 
+    //incoming messages
     client.onmessage = (message) => {
       const timestamp = parseInt(message.data);
-      // console.log("timestamp:", timestamp); 
-      // console.log("date.now:",Date.now());
       const currentTimestamp = Date.now();
       const packetLatency = currentTimestamp - timestamp;
-      // console.log("packetlatency:", packetLatency);
       setLatency(packetLatency);
       setLatencyStatus("green");
+      // client.close(); // close connection - to add flicker effect
     };
-
+    
+    //closing messages
     client.onclose = () => {
-      setLatencyStatus("red");
+      setLatencyStatus("off");
     };
-  }, []);
+  }, [latencyStatus]);
 
   return { latency,  latencyStatus };
 }
