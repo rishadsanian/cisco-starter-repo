46f043ddee04615afe2d727b86b09e8f2fa029df
Rishad Alam <rishad.s.a@gmail.com>
2023-10-26 00:04:30 -0400

    eg1

--- sextant/src/components/Banner.jsx
+++ sextant/src/components/Banner.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import './Banner.css';
+// import ciscoLogo from './Cisco_logo_blue_2016.svg';
+
+
 
 
 function Banner() {
-  return <h1 className="banner">SEXTANT</h1>;
-
+  return (
+    <div>
+      <div>
+      {/* <img src={ciscoLogo} alt="Cisco logo" /> */}
+        
+        <h1 className="banner">SEXTANT</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
+
--- sextant/src/components/Exhibit.jsx
+++ sextant/src/components/Exhibit.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import './Exhibit.css';
+import React from "react";
+import "./Exhibit.css";
 
 function Exhibit({ widget }) {
   return (
     <div className="widget">
       <h2 className="heading">{widget.heading}</h2>
       <p className="value">{widget.data}</p>
-    </div> 
+    </div>
   );
 }
 
 export default Exhibit;
-
-
-
